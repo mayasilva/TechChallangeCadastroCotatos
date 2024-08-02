4b29e4ece59050e588586358821ff74e3bc2115d
Mayara Alves da Silva <mayara.silva28@gmail.com>
2024-08-02 15:12:00 -0300

    Adiciona projeto de Core para criar as entidades no modelo DDD

---
+++ Core/Entity/Contato.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Core.Entity
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public required int DDD { get; set; }
+        public string Regiao { get; set; }
+        public int Telefone { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
