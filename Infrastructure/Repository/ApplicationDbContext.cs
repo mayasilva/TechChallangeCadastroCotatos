2a7cf84df5dab5eb45d07a72fb43cbfff7f06571
Mayara Alves da Silva <mayara.silva28@gmail.com>
2024-08-02 15:38:41 -0300

    adiciona projeto de infrastrutura, com a configuração do dbcontext e o dbset da tabela de contato

---
+++ Infrastructure/Repository/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public ApplicationDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DbSet<Contato> Contato { get; set; }
+    }
+}
