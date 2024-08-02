0756690e01caede1d43580b7b2355ea99813c996
Mayara Alves da Silva <mayara.silva28@gmail.com>
2024-08-02 17:36:24 -0300

    retira campo de região

--- Core/Entity/Contato.cs
+++ Core/Entity/Contato.cs
@@ -7,9 +7,8 @@ namespace Core.Entity
         public int Id { get; set; }
         public string Nome { get; set; }
         public required int DDD { get; set; }
-        public string Regiao { get; set; }
         public int Telefone { get; set; }
         public string Email { get; set; }
-        
+
     }
 }
