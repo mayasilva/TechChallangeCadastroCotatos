9056851ca3c1b599ab6105519908443c6ee7b0a8
Mayara Alves da Silva <mayara.silva28@gmail.com>
2024-08-06 11:50:41 -0300

    cria teste unitario da controller

---
+++ Test/Unitario/ContatoControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entity;
+using Core.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TechChallangeCadastroContatosAPI.Controllers;
+
+namespace Test.Unitario
+{
+    public class ContatoControllerTest
+    {
+        [Fact]
+        public void Get_DeveReotrnar200ComUmaListaVazia_QuandoRepositoryDevolverVazio()
+        {
+            var mockContatoRepository = new Mock<IContatoRepository>();
+            mockContatoRepository.Setup(repository => repository.ObterTodos()).Returns(new List<Contato>());
+
+            var contatoController = new ContatoController(mockContatoRepository.Object);
+
+            var resultado = Assert.IsType<OkObjectResult>(contatoController.Get());
+            Assert.NotNull(resultado);
+            Assert.Equal(StatusCodes.Status200OK, resultado.StatusCode);
+            var valor = Assert.IsType<List<Contato>>(resultado.Value);
+            Assert.Empty(valor);
+        }
+    }
+}
