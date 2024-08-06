3c7b0e905bbef3b279375e47a429dc33a5d3fcc0
Mayara Alves da Silva <mayara.silva28@gmail.com>
2024-08-06 14:01:34 -0300

    Adiciona mais teste na controller

--- Test/Unitario/ContatoControllerTest.cs
+++ Test/Unitario/ContatoControllerTest.cs
@@ -23,5 +23,51 @@ namespace Test.Unitario
             var valor = Assert.IsType<List<Contato>>(resultado.Value);
             Assert.Empty(valor);
         }
+
+        [Fact]
+        public void Get_DeveReotrnarBadRequest_QuandoDerUmaExeção()
+        {
+            var mockContatoRepository = new Mock<IContatoRepository>();
+            mockContatoRepository.Setup(repository => repository.ObterTodos()).Throws<System.IO.IOException>();
+
+
+            var contatoController = new ContatoController(mockContatoRepository.Object);
+
+            var resultado = Assert.IsType<BadRequestObjectResult>(contatoController.Get());
+            Assert.Equal(StatusCodes.Status400BadRequest, resultado.StatusCode);
+
+        }
+        [Fact]
+        public void GetDDD_DeveReotrnar200_QuandoRepositoryForChamadoObterPorDD()
+        {
+            //var contatoRetorno = new Contato()
+            //{
+            //    Nome = "Jõao",
+            //    DDD = 11,
+            //    Telefone = 999990000,
+            //    Email = "joao@gmail.com",
+            //};
+            //var contatoErro = new Contato()
+            //{
+            //    Nome = "Maria",
+            //    DDD = 12,
+            //    Telefone = 999990001,
+            //    Email = "Maria@gmail.com",
+            //};
+
+            var mockContatoRepository = new Mock<IContatoRepository>();
+            mockContatoRepository.Setup(repository => repository.ObterTodos()).Returns(new List<Contato>());
+
+            var contatoController = new ContatoController(mockContatoRepository.Object);
+            var resultado = Assert.IsType<OkObjectResult>(contatoController.GetPorDDD(11));
+            Assert.NotNull(resultado);
+            //Assert.Equal(StatusCodes.Status200OK, resultado.StatusCode);
+            //var resultadoContato = Assert.IsType<List<Contato>>(contatoController.GetPorDDD(11));
+            //Assert.Equal(contatoRetorno.Nome, contatoRetornado[0].Nome);
+            //Assert.Equal(contatoRetorno.DDD, contatoRetornado[0].DDD);
+            //Assert.Equal(contatoRetorno.Telefone, contatoRetornado[0].Telefone);
+            //Assert.Equal(contatoRetorno.Email, contatoRetornado[0].Email);
+        }
+
     }
 }
