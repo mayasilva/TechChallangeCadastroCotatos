4b981ff9eb61740403f2f951fa8482f3dcab6290
Felipe Gonçalves <lipegn2@gmail.com>
2024-08-06 20:33:09 -0300

    Validação annotation de login

--- Core/Input/LoginInput.cs
+++ Core/Input/LoginInput.cs
@@ -5,9 +5,11 @@ namespace Core.Input
     public class LoginInput
     {
         [Required(ErrorMessage = "Campo usuário é obrigatório")]
+        [DeniedValues("string", ErrorMessage = "Texto 'string' não é permitido")]
         public required string Usuario { get; set; }
 
         [Required(ErrorMessage = "Campo senha é obrigatório")]
+        [DeniedValues("string", ErrorMessage = "Texto 'string' não é permitido")]
         public required string Senha { get; set; }
     }
 }
